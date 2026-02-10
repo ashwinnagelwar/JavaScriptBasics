9c6bfbb0b7f2f5de06e4a3071938010b877680fd
ashwinnagelwar <ashwinnagelwar@gmail.com>
2026-02-11 00:05:04 +0530

    changes added

--- OOP/classes.js
+++ OOP/classes.js
@@ -1,16 +1,16 @@
-class User{
-    constructor(firstName, lastName, email){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }   
+// class User{
+//     constructor(firstName, lastName, email){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.email = email;
+//     }   
 
-    getFullName(){
-        return this.firstName + " " + this.lastName;
-    }
-}
+//     getFullName(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
 
-const user=new User("John", "Doe", 32);
+// const user=new User("John", "Doe", 32);
 
 // user
 // User {firstName: 'John', lastName: 'Doe', email: 32}
@@ -23,3 +23,67 @@ const user=new User("John", "Doe", 32);
 // undefined
 // user1.getFullName()
 // 'Abhi Shek'
+
+
+//Private properties and methods
+// class User1{
+    
+//     #age    
+//     constructor(firstName, lastName, age){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.#age = age;
+//     }
+
+//     #hi='Hello'
+//     getAge(){
+//         return this.#age;
+//     }
+// }   
+
+// const user1=new User1("John", "Doe", 32);
+
+//static property
+//we can directly access static property using class name without creating object of class
+
+// class User2{
+//     static count=0  
+//     constructor(firstName, lastName){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         User2.count++;
+//     }
+
+//     //static block
+//     static {
+//         console.log("Static block executed");
+//     }
+
+//     //static method 
+//     static getCount(){
+//         return User2.count++;
+//     }
+
+// }
+
+//Getters and Setters
+   
+const user={
+    firstName: "John",
+    lastName: "Doe",
+    get fullName(){
+        return this.firstName + " " + this.lastName;
+    },
+
+    //at console do below
+    //user.fullName-- It will return Jhone Doe
+    set fullName(name){
+        console.log(name);
+    }
+    //at consoel do below
+    //user.fullName="Ashwin Nagelwar"-- It will print Ashwin Nagelwar in console  
+    
+}
+
+
+
